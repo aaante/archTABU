70e722bbec4f2b945b386bb50db3ab330e939d35
Ante Sušić <aaaaaaaante@gmail.com>
2024-03-27 23:27:57 +0100

    refactored: SQLinsert.js

--- server/js/SQLinsert.js
+++ server/js/SQLinsert.js
@@ -1,6 +1,6 @@
 import { SQLclient } from "./SQLclient.js";
 
-async function SQLinsert() {
+async function SQLinsert(name) {
     try {
         await SQLclient.connect();
         console.log("Connected successfully");
@@ -8,15 +8,14 @@ async function SQLinsert() {
         await SQLclient.query("BEGIN;");
         console.log("BEGIN initiates a transactin block");
 
-        // Query for MAX number of name_id to get new ID
-        const results = await SQLclient.query("SELECT MAX(name_id) FROM names;");
-        const newID = results.rows[0].max + 1;
+        const name_id = await IDgenerator("name_id", "names");
 
-        await SQLclient.query("INSERT INTO names VALUES ($1, $2);", [
-            newID,
-            "Stipe",
-        ]);
+        const returning = await SQLclient.query(
+            "INSERT INTO names VALUES ($1, $2) RETURNING name_id;",
+            [name_id, name],
+        );
         console.log("Inserted new row");
+        console.log(returning);
 
         await SQLclient.query("COMMIT;");
         console.log("COMMIT terminates transaction block");
@@ -31,6 +30,14 @@ async function SQLinsert() {
     }
 }
 
-SQLinsert();
+// Query for MAX id number to get id number + 1
+async function IDgenerator(idName, tableName) {
+    const MAXid = await SQLclient.query(
+        `SELECT MAX(${idName}) FROM ${tableName};`,
+    );
+    return MAXid.rows[0].max + 1;
+}
+
+SQLinsert("Jole");
 
 export { SQLinsert };
