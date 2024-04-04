1be6a2c0679f5d292be0adfc7cb406f0bd9346de
Ante Sušić <aaaaaaaante@gmail.com>
2024-04-04 23:36:26 +0200

    new route that serves index.html; added req.params to get route

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
+// Import essentials
 import express from "express";
 const app = express();
 const port = 3000;
+const __dirname = import.meta.dirname;
 
 // Import model functions
 import { SQLinsert } from "./models/SQLinsert.js";
 import { SQLselect } from "./models/SQLselect.js";
 
-app.post("/", async (req, res) => {
-    try {
-        await SQLinsert("Josip", 3, 10000);
-    } catch (error) {
-        console.error(error);
-    } finally {
-        res.send(`<h1>Architect inserted into database!</h1>`);
+app.use(express.static(__dirname + "/static"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/:avg", async (req, res) => {
+    console.log(req.params);
+    if (req.params.avg === "avg") {
+        let avg;
+        try {
+            avg = await SQLselect();
+        } catch (error) {
+            console.error(error);
+        } finally {
+            res.send(`<h1>Average salary: ${avg}</h1>`);
+        }
+    } else {
+        res.send(`<h1>No salaries</h1>`);
     }
 });
 
-app.get("/", async (req, res) => {
-    let avg;
-    try {
-        avg = await SQLselect();
-    } catch (error) {
-        console.error(error);
-    } finally {
-        res.send(`<h1>Average salary: ${avg}</h1>`);
-    }
-});
+// app.post("/", async (req, res) => {
+//     try {
+//         await SQLinsert("Ante", 8, 12000);
+//     } catch (error) {
+//         console.error(error);
+//     } finally {
+//         res.send(`<h1>Architect inserted into database!</h1>`);
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`App listening on: http://localhost:${port}`);
