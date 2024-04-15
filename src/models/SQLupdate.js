7e06ad44771314e02b90a9b09a836c521b090e79
Ante Sušić <aaaaaaaante@gmail.com>
2024-04-15 18:02:09 +0200

    WIP: SQLupdate.js

--- src/models/SQLupdate.js
+++ src/models/SQLupdate.js
@@ -3,7 +3,7 @@ import { getAll } from "./getAll.js";
 import { updateColumn } from "./updateColumn.js";
 import { getCount } from "./getCount.js";
 import { updateColumnV2 } from "./updateColumnV2.js";
-import { getSpecific } from "./getSpecific.js";
+import { deleteRow } from "./deleteRow.js";
 
 async function SQLupdate(person_id, name, experience, salary) {
     console.log(`Updating person with person_id: ${person_id}`);
@@ -50,6 +50,7 @@ async function SQLupdate(person_id, name, experience, salary) {
 
         /* If name doesn't already exist in names table, update names table */
         if (counts.nameCount === 0) {
+            console.log("--- New name ---");
             let updatedName = await updateColumn(
                 "names",
                 "name",
@@ -64,6 +65,7 @@ async function SQLupdate(person_id, name, experience, salary) {
         /* If experience doesn't already exist in experience table, update
         experience table */
         if (counts.experienceCount === 0) {
+            console.log("--- New experience ---");
             let updatedExperience = await updateColumn(
                 "experience",
                 "experience",
@@ -78,6 +80,7 @@ async function SQLupdate(person_id, name, experience, salary) {
         /* If salary doesn't already exist in salaries table, update
         salaries table */
         if (counts.salaryCount === 0) {
+            console.log("--- New salary ---");
             let updatedSalary = await updateColumn(
                 "salaries",
                 "salary",
@@ -121,6 +124,7 @@ async function SQLupdate(person_id, name, experience, salary) {
         /* If name_id is not unique in people table (count of name_id is > 1),
         update name_id in people table to name_id of (new) name */
         if (IDcounts.nameIDcount > 1) {
+            console.log("--- Name is not unique ---");
             let updatedNameID = await updateColumnV2(
                 "people",
                 "name_id",
@@ -134,9 +138,7 @@ async function SQLupdate(person_id, name, experience, salary) {
             updatedNameID = updatedNameID.name_id;
             console.log(`updatedNameID: ${updatedNameID}`);
         } else {
-            /* Get name_id of current (to be changed) name from names table */
-            // DONE at the beggining of function
-
+            console.log("--- Name is unique ---");
             /* Update name_id in people table to names_id of (new) name */
             let updatedNameID = await updateColumnV2(
                 "people",
@@ -153,13 +155,21 @@ async function SQLupdate(person_id, name, experience, salary) {
 
             /* Delete row in names table where name is old name (name that
             was updated) */
-            // TODO
+            let deletedNameID = await deleteRow(
+                "names",
+                "name_id",
+                "name_id",
+                nameID,
+            );
+            deletedNameID = deletedNameID.name_id;
+            console.log(`deletedNameID: ${deletedNameID}`);
         }
 
         /* If experience_id is not unique in people table (count of
         experience_id is > 1), update experience_id in people table to
         experience_id of (new) experience */
         if (IDcounts.experienceIDcount > 1) {
+            console.log("--- Experience is not unique ---");
             let updatedExperienceID = await updateColumnV2(
                 "people",
                 "experience_id",
@@ -172,12 +182,40 @@ async function SQLupdate(person_id, name, experience, salary) {
             );
             updatedExperienceID = updatedExperienceID.experience_id;
             console.log(`updatedExperienceID: ${updatedExperienceID}`);
+        } else {
+            console.log("--- Experience is unique ---");
+            /* Update experience_id in people table to experience_id of
+            (new) experience */
+            let updatedExperienceID = await updateColumnV2(
+                "people",
+                "experience_id",
+                "experience_id",
+                "experience",
+                "experience",
+                "person_id",
+                experience,
+                person_id,
+            );
+            updatedExperienceID = updatedExperienceID.experience_id;
+            console.log(`updatedExperienceID: ${updatedExperienceID}`);
+
+            /* Delete row in experience table where experience is old
+            experience (experience that was updated) */
+            let deletedExperienceID = await deleteRow(
+                "experience",
+                "experience_id",
+                "experience_id",
+                experienceID,
+            );
+            deletedExperienceID = deletedExperienceID.experience_id;
+            console.log(`deletedExperienceID: ${deletedExperienceID}`);
         }
 
         /* If salary_id is not unique in people table (count of salary_id
         is > 1), update salary_id in people table to salary_id of (new)
         salary */
         if (IDcounts.salaryIDcount > 1) {
+            console.log("--- Salary is not unique ---");
             let updatedSalaryID = await updateColumnV2(
                 "people",
                 "salary_id",
@@ -190,8 +228,10 @@ async function SQLupdate(person_id, name, experience, salary) {
             );
             updatedSalaryID = updatedSalaryID.salary_id;
             console.log(`updatedSalaryID: ${updatedSalaryID}`);
+        } else {
+            console.log("--- Salary is unique ---");
+            // TODO
         }
-
         await SQLpool.query("COMMIT;");
         console.log("COMMIT terminates transaction block");
     } catch (ex) {
--- src/models/deleteRow.js
+++ src/models/deleteRow.js
@@ -1,3 +1,15 @@
 import { SQLpool } from "../configs/SQLpool.js";
 
+/* Delete row from table */
+/* Should be called inside transaction block */
+async function deleteRow(tableName, columnName1, columnName2, value) {
+    const result = await SQLpool.query(
+        `DELETE FROM ${tableName} ` +
+        `WHERE ${columnName1} = $1 ` +
+        `RETURNING ${columnName2};`,
+        [value],
+    );
+    return result.rows[0];
+}
+
 export { deleteRow };
