1f3fc4d922e95c0f61c4c865a270a2f7a37d4dc2
Ante Sušić <aaaaaaaante@gmail.com>
2024-04-21 18:24:23 +0200

    frontend: copied index.html from backend to app/page.js

---
+++ frontend/src/app/fuckAround.js
@@ -0,0 +1,10 @@
+import * as pkg from "next/font/google/index.js";
+
+console.log(pkg);
+
+// const { Inter } = pkg;
+// 
+// const inter = Inter({ subsets: ["latin"] });
+// 
+// console.log(Inter);
+// console.log(inter);
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,95 +1,93 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main>
+            <h1>archTABU 🏛️</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            {/* POST data */}
+            <form id="post" method="POST">
+                <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    value=""
+                    placeholder="name"
+                />
+                <br />
+                <input
+                    id="experience"
+                    type="number"
+                    name="experience"
+                    value=""
+                    placeholder="experience"
+                />
+                <br />
+                <input
+                    id="salary"
+                    type="number"
+                    name="salary"
+                    value=""
+                    placeholder="salary"
+                />
+                <br />
+                <input type="submit" value="POST data" />
+                <h2 id="new-insert"></h2>
+            </form>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            {/* GET average salary */}
+            <button id="get">GET average salary</button>
+            <h2 id="avg-salary"></h2>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            {/* UPDATE data */}
+            <form id="update" method="POST">
+                <input
+                    id="id"
+                    type="number"
+                    name="id"
+                    value=""
+                    placeholder="id"
+                />
+                <br />
+                <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    value=""
+                    placeholder="name"
+                />
+                <br />
+                <input
+                    id="experience"
+                    type="number"
+                    name="experience"
+                    value=""
+                    placeholder="experience"
+                />
+                <br />
+                <input
+                    id="salary"
+                    type="number"
+                    name="salary"
+                    value=""
+                    placeholder="salary"
+                />
+                <br />
+                <input type="submit" value="UPDATE data" />
+                <h2 id="new-update"></h2>
+            </form>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            {/* DELETE data */}
+            <form id="del" method="POST">
+                <input
+                    id="id"
+                    type="number"
+                    name="id"
+                    value=""
+                    placeholder="id"
+                />
+                <br />
+                <input type="submit" value="DELETE data" />
+                <h2 id="new-delete"></h2>
+            </form>
+        </main>
+    );
 }
